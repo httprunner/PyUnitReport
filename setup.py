d6d96aa900fc6edd0963cab12d12a181d3d4b4fd
httprunner <httprunner>
2017-07-19 17:44:14 +0800

    make simple

--- setup.py
+++ setup.py
@@ -1,101 +1,44 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-===============================
-HtmlTestRunner
-===============================
-
-
-.. image:: https://img.shields.io/pypi/v/html-testRunner.svg
-        :target: https://pypi.python.org/pypi/html-testRunner
-
-.. image:: https://img.shields.io/travis/oldani/HtmlTestRunner.svg
-        :target: https://travis-ci.org/oldani/HtmlTestRunner
-
-
-
-HtmlTest runner is a unittest test runner that save test results
-in Html files, for human readable presentation of results.
-
-This Package was inspired in ``unittest-xml-reporting`` and
-``HtmlTestRunner by tungwaiyip``.
-
-Usage:
---------------
-
-.. code-block:: python
-
-    import HtmlTestRunner
-    import unittest
-
-
-    class TestStringMethods(unittest.TestCase):
-
-        def test_upper(self):
-            self.assertEqual('foo'.upper(), 'FOO')
-
-        def test_error(self):
-            "\"" This test should be marked as error one. ""\"
-            raise ValueError
-
-        def test_fail(self):
-            "\"" This test should fail. ""\"
-            self.assertEqual(1, 2)
-
-        @unittest.skip("This is a skipped test.")
-        def test_skip(self):
-            "\"" This test should be skipped. ""\"
-            pass
-
-    if __name__ == '__main__':
-        unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='example_dir'))
-
-As simple as import the class an initialize it, it only have one request
-parameter that is output, this one is use to place the report in a sub
-direcotry in ``reports`` directory.
-
-Links:
----------
-
-* `Github <https://github.com/oldani/HtmlTestRunner>`_
-"""
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 requirements = [
     # Package requirements here
-    "Jinja2==2.9.5"
+    "Jinja2"
 ]
 
 test_requirements = [
     # Package test requirements here
 ]
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
-    name='html-testRunner',
-    version='1.1.0',
-    description="A Test Runner in python, for Human Readable HTML Reports",
-    long_description=__doc__,
-    author="Ordanis Sanchez Suero",
-    author_email='ordanisanchez@gmail.com',
-    url='https://github.com/oldani/HtmlTestRunner',
-    packages=[
-        'HtmlTestRunner',
-    ],
-    package_dir={'HtmlTestRunner':
-                 'HtmlTestRunner'},
+    name='PyUnitReport',
+    version='0.1.0',
+    description="A unit test runner for Python, and generate HTML reports.",
+    long_description=long_description,
+    author="Ordanis Sanchez Suero, Leo Lee",
+    author_email='mail@debugtalk.com',
+    url='https://github.com/debugtalk/PyUnitReport',
+    packages=find_packages(exclude=['tests']),
+    package_data={
+        'PyUnitReport': ['template/*'],
+    },
     include_package_data=True,
     install_requires=requirements,
     license="MIT license",
     zip_safe=False,
     keywords='HtmlTestRunner TestRunner Html Reports',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        "Development Status :: 3 - Alpha",
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     test_suite='tests',
     tests_require=test_requirements
---
+++ tests/test_html.py
@@ -0,0 +1,19 @@
+"""
+Tests for `PyUnitReport` module.
+"""
+
+import unittest
+
+import PyUnitReport
+
+
+class TestPyUnitReport(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
