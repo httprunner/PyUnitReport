dd903b3dff934f1e254ba5480b6a85c2bb73f9ad
Banrieen <banrieen@163.com>
2018-04-04 14:25:48 +0800

    modify the testcasename title as default strings "Testcases Name"

--- pyunitreport/runner.py
+++ pyunitreport/runner.py
@@ -11,13 +11,13 @@ class HTMLTestRunner(TextTestRunner):
     """" A test runner class that output the results. """
 
     def __init__(self, output, report_name=None, verbosity=2, stream=sys.stderr,
-                 descriptions=True, failfast=False, buffer=False,testsuitsName=None,
+                 descriptions=True, failfast=False, buffer=False,
                  report_title=None, template=None, resultclass=None):
         self.verbosity = verbosity
         self.output = output
         self.report_name = report_name
         self.encoding = UTF8
-        self.testsuitsName = testsuitsName
+        self.testcase_name = "Testcases Name"
 
         TextTestRunner.__init__(self, stream, descriptions, verbosity,
                                 failfast=failfast, buffer=buffer)
